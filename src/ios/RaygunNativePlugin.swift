ba35526fbab5b0f5b803619bec213216494271e4
James Spencer <thisjustosay@gmail.com>
2015-05-27 12:23:04 -0600

    stub init code iOS

--- src/ios/RaygunNativePlugin.swift
+++ src/ios/RaygunNativePlugin.swift
@@ -4,6 +4,8 @@ import UIKit
 @objc(RaygunNativePlugin) class RaygunNativePlugin : CDVPlugin  {
 
     func startNativeRaygun(command: CDVInvokedUrlCommand) {
+    //[Raygun sharedReporterWithApiKey:@"YOUR_APP_API_KEY"];
+    //[[Raygun sharedReporter] identify:@"UNIQUE_USER_IDENTITY"];
         //options: NSMutableDictionary =
         //NSMutableDictionary *options = [command argumentAtIndex:0];
         //    NSString *identifier = [options objectForKey:@"value"];
