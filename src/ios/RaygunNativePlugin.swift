6528b03180b35dcb0f56006145daed004bc1a9fd
James Spencer <thisjustosay@gmail.com>
2016-09-17 10:41:20 -0600

    update to swift 3 syntax

--- src/ios/RaygunNativePlugin.swift
+++ src/ios/RaygunNativePlugin.swift
@@ -3,15 +3,15 @@ import UIKit
 import Raygun4iOS
 
 @objc(RaygunNativePlugin) class RaygunNativePlugin : CDVPlugin  {
-    func startNativeRaygun(command: CDVInvokedUrlCommand) {
+    func startNativeRaygun(_ command: CDVInvokedUrlCommand) {
         let params: AnyObject = command.arguments[0] as AnyObject!
-        let user: String = params["user"] as AnyObject! as! String
-        let apiKey: String = params["api"] as AnyObject! as! String
-        Raygun.sharedReporterWithApiKey(apiKey)
-        Raygun.sharedReporter().identify(user)
+        let user: String = params["user"] as! String
+        let apiKey: String = params["api"] as! String
+        Raygun.sharedReporter(withApiKey: apiKey)
+        (Raygun.sharedReporter() as AnyObject).identify(user)
     }
 
-    func testCrash(command: CDVInvokedUrlCommand) {
+    func testCrash(_ command: CDVInvokedUrlCommand) {
         var crashWithMissingValueInDicitonary = Dictionary<Int,Int>()
         _ = crashWithMissingValueInDicitonary[1]!
     }
