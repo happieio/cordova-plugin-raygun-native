f3f117d60a518c1114709fca683daa382db0b567
James Spencer <thisjustosay@gmail.com>
2017-09-21 08:32:15 -0700

    Revert "comment the native raygun provider for now"
    This reverts commit 555c5979072bfad1f726e06376bf4812aa87393b.

--- src/ios/RaygunNativePlugin.swift
+++ src/ios/RaygunNativePlugin.swift
@@ -1,14 +1,14 @@
 import Foundation
 import UIKit
-//import Raygun4iOS
+import Raygun4iOS
 
 @objc(RaygunNativePlugin) class RaygunNativePlugin : CDVPlugin  {
     func startNativeRaygun(_ command: CDVInvokedUrlCommand) {
-//        let params: [String: Any] = command.arguments[0] as! [String: Any]
-//        let user: String = params["user"] as! String
-//        let apiKey: String = params["api"] as! String
-//        Raygun.sharedReporter(withApiKey: apiKey)
-//        (Raygun.sharedReporter() as AnyObject).identify(user)
+        let params: [String: Any] = command.arguments[0] as! [String: Any]
+        let user: String = params["user"] as! String
+        let apiKey: String = params["api"] as! String
+        Raygun.sharedReporter(withApiKey: apiKey)
+        (Raygun.sharedReporter() as AnyObject).identify(user)
     }
 
     func testCrash(_ command: CDVInvokedUrlCommand) {
