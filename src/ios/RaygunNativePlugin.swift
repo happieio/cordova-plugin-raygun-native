f1504392614856c0faa2cbf60b4b89cca854e063
James Spencer <thisjustosay@gmail.com>
2017-09-29 12:09:16 -0700

    comment iOS native

--- src/ios/RaygunNativePlugin.swift
+++ src/ios/RaygunNativePlugin.swift
@@ -4,15 +4,15 @@ import Raygun4iOS
 
 @objc(RaygunNativePlugin) class RaygunNativePlugin : CDVPlugin  {
     func startNativeRaygun(_ command: CDVInvokedUrlCommand) {
-        let params: [String: Any] = command.arguments[0] as! [String: Any]
-        let user: String = params["user"] as! String
-        let apiKey: String = params["api"] as! String
-        Raygun.sharedReporter(withApiKey: apiKey)
-        (Raygun.sharedReporter() as AnyObject).identify(user)
+//        let params: [String: Any] = command.arguments[0] as! [String: Any]
+//        let user: String = params["user"] as! String
+//        let apiKey: String = params["api"] as! String
+//        Raygun.sharedReporter(withApiKey: apiKey)
+//        (Raygun.sharedReporter() as AnyObject).identify(user)
     }
 
     func testCrash(_ command: CDVInvokedUrlCommand) {
-        var crashWithMissingValueInDicitonary = Dictionary<Int,Int>()
-        _ = crashWithMissingValueInDicitonary[1]!
+//        var crashWithMissingValueInDicitonary = Dictionary<Int,Int>()
+//        _ = crashWithMissingValueInDicitonary[1]!
     }
 }
